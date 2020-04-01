f6a0f5984132b732ac6aa6a0d4cf10245596ddfb
Alan Spinola <alanjuniornunu8@gmail.com>
2020-03-31 22:19:15 -0300

    Simular SelectionSort

---
+++ src/Cartas.java
@@ -0,0 +1,23 @@
+
+public class Cartas {
+	private String carta;
+	private int valor;
+	
+
+
+	public Cartas (String carta, int valor) {
+		this.carta = carta;
+		this.valor = valor;
+		
+		
+	}
+
+	public String getCarta() {
+		return carta;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+	
+}
---
+++ src/TestaOrdenaCartas.java
@@ -0,0 +1,40 @@
+
+public class TestaOrdenaCartas {
+	
+	public static void main(String[] args) {
+		
+		Cartas maoCartas[] = {
+			new Cartas("Copas", 4),
+			new Cartas("Copas", 7),
+			new Cartas("Copas", 10),
+			new Cartas("Espadas", 0),
+			new Cartas("Ouros", 8)	
+		};
+		
+	ordenaCartas(maoCartas, maoCartas.length);
+	
+	for(Cartas carta : maoCartas) {
+		System.out.println(carta.getValor() + " de " + carta.getCarta());
+	}
+	
+	
+	}
+
+	
+	
+	public static void ordenaCartas(Cartas[] cartas, int numeroDeCartas) {
+		int cartaMenor = 0;
+		for(int carta = 0; carta < numeroDeCartas - 1; carta++) {
+			for(int cartaAtual = carta; cartaAtual < numeroDeCartas; cartaAtual++ ){
+				if( cartas[cartaAtual].getValor() < cartas[cartaMenor].getValor()) {
+					cartaMenor = cartaAtual;
+				}
+			}
+
+		Cartas atual = cartas[carta];
+		Cartas menor = cartas[cartaMenor];
+		cartas[carta] = menor ;
+		cartas[cartaMenor] = atual ;
+		}
+	}
+}
